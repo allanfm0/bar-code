8f574ad362b5df33396ebe706fd6a9b698f913e2
Allan <allanfelipedk@gmail.com>
2024-02-07 20:16:17 -0300

    feat: Implementing App in Src

--- run_raw.py
+++ run_raw.py
@@ -4,6 +4,7 @@ from barcode.writer import ImageWriter
 
 app = Flask(__name__)
 
+
 @app.route('/create_tag', methods=["POST"])
 def create_tag():
     body = request.json
@@ -13,7 +14,8 @@ def create_tag():
     path_from_tag = f'{tag}'
     tag.save(path_from_tag)
 
-    return jsonify({ "tag_path": path_from_tag })
+    return jsonify({"tag_path": path_from_tag})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
